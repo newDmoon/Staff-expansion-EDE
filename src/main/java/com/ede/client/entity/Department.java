7b862d6a37763dfac77e1d336a66d9ad36759cc6
Dmitry <20125dimanovogrodsky@gmail.com>
2021-12-26 20:05:03 +0300

    added entity classes

--- src/main/java/com/ede/client/entity/Department.java
+++ src/main/java/com/ede/client/entity/Department.java
@@ -1,5 +1,58 @@
 package com.ede.client.entity;
 
-public class Department {
+import java.util.ArrayList;
 
+public class Department {
+    long id; // cчетчик
+    String information; // длинный текст
+    int countOfEmployees; // числовой
+    ArrayList<Employee> employees; // связь в бд, айди и фио
+
+    public Department(String information, int countOfEmployees, ArrayList<Employee> employees) {
+        this.information = information;
+        this.countOfEmployees = countOfEmployees;
+        this.employees = employees;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", information='" + information + '\'' +
+                ", countOfEmployees=" + countOfEmployees +
+                ", employees=" + employees +
+                '}';
+    }
+
+    public Department(ArrayList<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public String getInformation() {
+        return information;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+
+    public int getCountOfEmployees() {
+        return countOfEmployees;
+    }
+
+    public void setCountOfEmployees(int countOfEmployees) {
+        this.countOfEmployees = countOfEmployees;
+    }
+
+    public void setEmployees(ArrayList<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public ArrayList<Employee> getEmployees() {
+        return employees;
+    }
 }
